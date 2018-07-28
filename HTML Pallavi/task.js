e4d98d158cc816351f348d5c11def0a866d36f56
PallaviNiitCoreJava <37013933+PallaviNiitCoreJava@users.noreply.github.com>
2018-07-28 16:16:38 +0530

    Add files via upload

---
+++ HTML Pallavi/task.js
@@ -0,0 +1,9 @@
+	var a;
+	alert("Welcome");
+	function task(col)
+	{
+		//document.write("Welcome To JavaScript");
+		var d=document.getElementById('one');
+		d.innerText="Welcome To JavaScript";
+		d.setAttribute("style","border:1px solid black; display:inline;color:"+col+"");
+	}
